2e5b5eac3255187388dcb6dc47db23be2eecc1e6
ChiefCleef <nickvcg@yahoo.com>
2020-04-25 15:05:14 -0400

    _parser.py is the new parser class

---
+++ src/_parser.py
@@ -0,0 +1,386 @@
+"""
+Python Implementation of a Parser for a Subset of BASIC (ECMA 116 Standard)
+Created by Nihad Kalathingal and Nick Green on 3/19/2020. Modified (03-20-2020)
+    Kennesaw State University
+    College of Computing and Software Engineering
+    Department of Computer Science
+    4308 Concepts of Programming Languages 03
+    Module 3 – 2nd Deliverable
+    Nihad Kalathingal (nkalathi@students.kennesaw.edu)
+    Nick Green (ngreen@students.kennesaw.edu)
+"""
+
+from scanner import *   # import scanner and scanner errors
+from basic_subset import *  # import the basic subset with the tokens
+import sys  # import sys used for CLI args
+
+"""
+The parser is implemented as functions which uses the BNF grammar rules
+specified for the chosen Basic subset. The implementation is direct
+in the sense that each non-terminal in the grammar has a function.
+The parser uses the scanner to retrieve tokens specified by the Token
+in the lex() function. The parser throws a ParserError if an invalid
+statment is found.
+"""
+
+# global varibales used through parser functions
+#global lexer  # the generator of lexemes from the scanner
+#global next_token  # next token / look ahead from scanner
+#next_token = None
+#lexer = None
+
+class parser():
+
+    def __init__(self):
+        """
+        Simple constructor to initialize next_token and lexer.
+        """
+        next_token = None
+        lexer = None
+
+    class ParserError(Exception):
+        """
+        Exception class for a Parser error.
+        Used in case a parsing error occurs.
+        """
+        def __init__(self, pos: tuple, err=None):
+            """
+            Simple constructor to assign ParserError attributes.
+
+            Parameters:
+            pos (tuple): tuple of length 2 of the form (row, column)
+            pos (str): string description of an error, a generic error is used
+            if none is given
+            """
+
+            if err is None:
+                # use a default error if none specified
+                err = "Parsing error occured."
+            self.err = err
+            self.pos = pos  # position of error
+
+        def __str__(self):
+            """
+            Returns an error message with details of the error.
+            """
+            return "ParserError: {} Ln:{} Col:{}".format(self.err, self.pos[0],
+                                                         self.pos[1])
+
+
+    def program(self):
+        """
+        Starting point for the parser.
+        Function for the program non-terminal following the BNF rule:
+        <program> -> <statements>
+        """
+        # enter program
+        print("<program>")
+        # parse statements
+        self.statements()
+        # exit program
+        print("</program>")
+
+
+    def statements(self):
+        """
+        Function for the statements non-terminal following the BNF rule:
+        <statements> -> <statement>
+                      | <statement> EOL <statements>
+        """
+        # parse statemnt
+        self.lex()
+        self.statement()
+        # while mext token EOL parse statements
+        if self.next_token.type == Delimiters.EOL:
+            self.statements()
+
+
+    def statement(self):
+        """
+        Function for the statement non-terminal following the BNF rule:
+        <statement> -> <assn_stmnt>
+                    | <print_stmnt>
+                    | <do_while>
+                    | <if_stmnt>
+                    | END
+        """
+        # enter statement
+        print("<statement>")
+        # choose type of statement based on next_token
+        if self.next_token.type == Keywords.LET:
+            self.assn_stmnt()
+        elif self.next_token.type == Keywords.PRINT:
+            self.print_stmnt()
+        elif self.next_token.type == Keywords.DO:
+            self.do_while()
+        elif self.next_token.type == Keywords.IF:
+            self.if_stmnt()
+        elif self.next_token.type == Keywords.END:
+            # end of program/statement, do nothing
+            pass
+        elif self.next_token.type == Delimiters.EOL:
+            #  empty statement/line, do nothing
+            pass
+        else:
+            # raise a parsing error as its not a valid statement
+            raise self.ParserError(self.next_token.pos, "Invalid type of statement")
+        # exit statement
+        print("</statement>")
+
+
+    def assn_stmnt(self):
+        """
+        Function for the assn_stmnt non-terminal following the BNF rule:
+        <assn_stment> -> LET IDENT EQUAL_OP <expr>
+        """
+        # enter assig_stmnt
+        print("<assn_stmnt>")
+        # check for identifier
+        self.lex()
+        if self.next_token.type != Identifiers.IDENT:
+            raise self.ParserError(self.next_token.pos,
+                              "Invalid identifier in assignment statement")
+        # check for assinment operator
+        self.lex()
+        if self.next_token.type != Operators.EQUAL_OP:
+            raise self.ParserError(self.next_token.pos, "Invalid assignment statement")
+        # parse an expression
+        self.expr()
+        # exit assig_stmnt
+        print("</assn_stmnt>")
+
+
+    def expr(self):
+        """
+        Function for the expr non-terminal following the BNF rule:
+        <expr> -> <term> ADD_OP <expr>
+                | <term> SUB_OP <expr>
+                | <term>
+        """
+        # enter expr
+        print("<expr>")
+        # parse a term
+        self.term()
+        # check for addition or subtraction, if so parse the expression after
+        if self.next_token.type == Operators.ADD_OP:
+            self.expr()
+        elif self.next_token.type == Operators.SUB_OP:
+            self.expr()
+        # exit expression
+        print("</expr>")
+
+
+    def term(self):
+        """
+        Function for the term non-terminal following the BNF rule:
+        <term> -> <factor> MULT_OP <term>
+                | <factor> DIV_OP <term>
+                | <factor>
+        """
+        # enter term
+        print("<term>")
+        # parse a factor
+        self.factor()
+        # check for multiplication/division, if so parse another term
+        self.lex()
+        if self.next_token.type == Operators.MULT_OP:
+            self.term()
+        elif self.next_token.type == Operators.DIV_OP:
+            self.term()
+        # exit term
+        print("</term>")
+
+
+    def factor(self):
+        """
+        Function for the factor non-terminal following the BNF rule:
+        <factor> -> LEFT_PEREN<expr>RIGHT_PEREN | ID | FLOAT_LIT | INT_LIT
+        """
+        # enter factor
+        print("<factor>")
+        # look for parentheses
+        self.lex()
+        if self.next_token.type == Operators.LEFT_PEREN:
+            # parse an expression
+            self.expr()
+            # ensure parenthesis are balanced
+            if self.next_token.type != Operators.RIGHT_PEREN:
+                raise self.ParserError(self.next_token.pos, "Mismatched parenthesis")
+        # if identifier. int_lit, or float_lit, reached terminal, do nothing
+        elif self.next_token.type == Identifiers.IDENT:
+            pass
+        elif self.next_token.type == Literals.INT_LIT:
+            pass
+        elif self.next_token.type == Literals.FLOAT_LIT:
+            pass
+        else:
+            # raise an error if none of the above conditions matched
+            raise self.ParserError(self.next_token.pos, "Invalid factor")
+        # exit factor
+        print("</factor>")
+
+
+    def print_stmnt(self):
+        """
+        Function for the print_stmnt non-terminal following the BNF rule:
+        <print_stmnt> -> PRINT <expr>
+        """
+        # enter print
+        print("<print_stmnt>")
+        # parse expression
+        self.expr()
+        # exit print
+        print("</print_stmnt>")
+
+
+    def do_while(self):
+        """
+        Function for the do_while non-terminal following the BNF rule:
+        <do_while> -> DO WHILE <relational-expression> EOL <statement> LOOP
+        """
+        # enter do_while
+        print("<do_while>")
+        # check for while statement
+        self.lex()
+        # if no while raise an error
+        if self.next_token.type != Keywords.WHILE:
+            raise self.ParserError(self.next_token.pos, "Invalid loop")
+        # parse relational expression
+        self.relational_expr()
+        # parse the body
+        self.body()
+        # check for loop end else raise error
+        if self.next_token.type != Keywords.LOOP:
+            raise self.ParserError(self.next_token.pos, "Invalid loop")
+        # check for loop EOL else raise error
+        self.lex()
+        if self.next_token.type != Delimiters.EOL:
+            raise self.ParserError(self.next_token.pos, "Invalid loop")
+        # exit do_while
+        print("</do_while>")
+
+
+    def if_stmnt(self):
+        """
+        Function for the if_stmnt non-terminal following the BNF rule:
+        <if_stmnt> ->  IF <relational-expression> THEN EOL <body> IF EOL
+        """
+        # enter if_stmnt
+        print("<if_stmnt>")
+        # parse relational expression
+        self.relational_expr()
+        # check for if otherwise raise error
+        if self.next_token.type != Keywords.THEN:
+            raise self.ParserError(self.next_token.pos, "Invalid if statement")
+        # check for EOL, otherwise raise error
+        self.lex()
+        if self.next_token.type != Delimiters.EOL:
+            raise self.ParserError(self.next_token.pos, "Invalid if statement")
+        # parse the body
+        self.body()
+        # check for end if otherwise raise error
+        if self.next_token.type != Keywords.IF:
+            raise self.ParserError(self.next_token.pos, "Invalid if statement")
+        # check for EOL, otherwise raise error
+        self.lex()
+        if self.next_token.type != Delimiters.EOL:
+            raise self.ParserError(self.next_token.pos, "Invalid if statement")
+        # exit if_stmnt
+        print("</if_stmnt>")
+
+
+    def relational_expr(self):
+        """
+        Function for the relational_expr non-terminal following the BNF rule:
+        <relational-expression> -> <expr> EQUAL_OP <expr>
+                                    | <expr> LESS_THAN <expr>
+                                    | <expr> GREATER_THAN <expr>
+                                    | <expr> NOT_GREATER <expr>
+                                    | <expr> NOT_LESS <expr>
+        """
+        # enter relational_expr
+        print("<relational_expr>")
+        # parse the expression
+        self.expr()
+        # parse another expression if an operator is present
+        if self.next_token.type == Operators.EQUAL_OP:
+            self.expr()
+        elif self.next_token.type == Operators.LESS_THAN:
+            self.expr()
+        elif self.next_token.type == Operators.GREATER_THAN:
+            self.expr()
+        elif self.next_token.type == Operators.NOT_GREATER:
+            self.expr()
+        elif self.next_token.type == Operators.NOT_LESS:
+            self.expr()
+        # exit relational_expr
+        print("</relational_expr>")
+
+
+    def body(self):
+        """
+        Function for the body non-terminal following the BNF rule:
+        body -> <statement><body>
+            |<statement> IF
+            |<statement> LOOP
+        """
+        # enter body
+        print("<body>")
+        self.lex()
+        # parse statements while not end of loop/if statement
+        while(self.next_token.type != Keywords.IF and self.next_token.type != Keywords.LOOP):
+            self.statement()
+            self.lex()
+        # exit body
+        print("</body>")
+
+
+    def lex(self):
+        """
+        retrieves next token from scanner and assigns the next token to a globally
+        available variable, to be used by other parser functions.
+        """
+        try:
+            # get the next token
+            token = next(self.lexer)
+        except ScannerError as e:
+            # catch any errors and print them
+            print(e)
+        # print the token
+        print(token)
+        # make next token global and available to other functions
+        #global next_token
+        self.next_token = token
+
+    def main(self, file):
+        #if self.__name__ == "__main__":
+            '''
+            Ensure that the Python 3 interpreter is installed.
+            The parser can be used with BASIC file using the following command:
+                
+            python3 parser.py <filename>
+                
+            For example:
+            
+            python3 parser.py test.bas
+            
+            Ensure that the file is in the same folder as the script or provide an
+            a path to file.
+            '''
+            # get the filename from the first CLI argument
+            filename = file
+            # use with context manager to open/close file and use exception handling
+            with open(filename, "r") as f:
+                scanner = Scanner(f)  # create a scanner object with a source file
+                # make the generator global to be used with parser functions
+                self.lexer = scanner.lex()
+                # try catch to catch any parser errors
+                try:
+                    # start parsing the program
+                    self.program()
+                except self.ParserError as e:
+                    # if a parsing error occurred, alert the user
+                    print(e)
+                except Exception as e:
+                    # print any other errors
+                    print(e)
